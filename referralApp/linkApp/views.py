28209823e8b654c948a7de4b1a8adae7c9016e2e
Randell Bentley <randellbentley@gmail.com>
2019-02-05 20:31:31 -0700

    Added ability to add links

--- referralApp/linkApp/views.py
+++ referralApp/linkApp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import loader
 from linkApp.models import WebSite
@@ -8,8 +8,13 @@ def index(request):
 
 def linkList(request):
     link_referral_list = WebSite.objects.order_by('clicks')[:5]
-    template = loader.get_template('linkApp/home.html')
+    template = loader.get_template('linkApp/index.html')
     context = {
         'link_referral_list': link_referral_list,
     }
     return HttpResponse(template.render(context, request))
+
+def create(request):
+    website = WebSite(link_title=request.POST['link_title'])
+    website.save()
+    return redirect('/')
