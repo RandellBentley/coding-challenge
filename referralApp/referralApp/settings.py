6fcba90675ac84b5c90f6538141d6ba7da24d16a
Randell Bentley <randellbentley@gmail.com>
2019-02-08 13:01:16 -0700

    Added procfile for deployment

--- referralApp/referralApp/settings.py
+++ referralApp/referralApp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -129,3 +130,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, "linkApp/static")
+
+django_heroku.settings(locals())
